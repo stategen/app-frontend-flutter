4b2f96027d07e35e88c9dc18a44cb1f23d25053e
niaoge <78493244@qq.com>
2020-07-06 11:26:38 +0800

    flutter demo fix

--- lib/stgutil/app_config.dart
+++ lib/stgutil/app_config.dart
@@ -4,19 +4,21 @@ import '../intergrade/configs/tradeApp_config.dart';
 class AppConfig {
   static final String appid = "wx2f40ccdc0d1f9217";
 
-
-  static final int netwait = isInDebugMode()? 1000000:10000; //debug :1000秒，release:10秒
+  //debug :1000秒，release:10秒
+  static final int netwait = isInDebugMode() ? 1000000 : 10000;
   static final String universalLink = "https://your.univerallink.com/link/";
 
   static Map<String, String> BASE_URLS = {
     tradeAppBaseUrlKey: "http://192.168.43.100:8080/tradeApp/",
-    payRpcBaseUrlKey:"http://192.168.43.100:8080/payRpc/"
+
   };
 
 
   static bool isInDebugMode() {
     bool inDebugMode = false;
-    assert(inDebugMode = true);//如果debug模式下会触发赋值
+    //如果debug模式下会触发赋值
+
+    assert(inDebugMode = true);
     return inDebugMode;
   }
 }
